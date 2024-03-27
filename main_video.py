45a1eca3e50d043dec72f33b27521b702898bbeb
Dali <94851502+dalixMasmoudi@users.noreply.github.com>
2024-03-27 22:26:33 +0100

    Update main_video.py

--- main_video.py
+++ main_video.py
@@ -25,10 +25,10 @@ while True:
         current_time = datetime.now()
 
         print("Personne détectée :", name, "à l'heure suivante :", current_time.strftime("%H:%M:%S"))
-     #   y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
-      #  cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-       # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 200), 4)
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 200, 200), 4)
         ctime=time.time()
         print('Fps', 1/(ctime-ptime))
         ptime=ctime
